491497f0892803640a163d93bcc1eefdbc320a45
GrayChinchilla45 <69173208+GrayChinchilla45@users.noreply.github.com>
2020-09-22 16:55:19 -0700

    Moving Platforms need some fixing.

---
+++ Platformer -- copy/Assets/scripts/MovingPlatform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    private bool isMoving = false;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (isMoving)
+        {
+            rb.velocity = new Vector2(speed, 0.0f);
+        }
+      else
+        {
+            rb.velocity = new Vector2();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>() != null){
+            isMoving = true;
+
+        }
+    }
+}
